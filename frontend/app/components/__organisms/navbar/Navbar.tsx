88a4c1ff76983b425ee83e0398c99da48d65ca27
Andria Ghurtchumelia <121307641+ghuchumeliandria@users.noreply.github.com>
2025-06-15 13:23:12 +0400

    Add frontend and backend folders

---
+++ frontend/app/components/__organisms/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className='w-full max-w-[640px] mx-auto bg-[#FEFEFE] p-[7px] h-[52px] rounded-[10px]'>
+            <h1></h1>
+        </div>
+    )
+}
