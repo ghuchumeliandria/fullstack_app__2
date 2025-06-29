16322d42284ff73a7c34002d2213aabc85d88221
Andria Ghurtchumelia <121307641+ghuchumeliandria@users.noreply.github.com>
2025-06-29 21:19:49 +0400

    authorization update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin : process.env.FRONT_URL
+  })
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist : true,
     transform : true
