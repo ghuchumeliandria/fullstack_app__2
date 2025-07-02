97cc39d2f788f61e2a1fb174fd94eb109dbc8dfc
Andria Ghurtchumelia <121307641+ghuchumeliandria@users.noreply.github.com>
2025-07-02 13:55:29 +0400

    dsad

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,24 +3,39 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: process.env.FRONT_URL,
+//     credentials: true,
+//   });
+
+//   app.use(cookieParser());
+  
+  
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       transform: true,
+//     }),
+//   );
+
+//   await app.listen(process.env.PORT ?? 3001);
+// }
+// bootstrap();
+
+// main.ts
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONT_URL,
+    origin: ['https://fullstack-app-2-vq...vercel.app'], 
     credentials: true,
   });
 
-  app.use(cookieParser());
-  
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
-
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(3000);
 }
 bootstrap();
